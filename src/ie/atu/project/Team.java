2fc58879c3341ef7a5d99d62db2953d01f697f70
DevlinOT <148062566+DevlinOT@users.noreply.github.com>
2024-12-04 12:13:05 +0000

    variables

--- src/ie/atu/project/Team.java
+++ src/ie/atu/project/Team.java
@@ -1,5 +1,17 @@
 package ie.atu.project;
 
 public class Team {
+    private String teamName;
+    private String country;
+    private String foundYear;
+
+    public Team(String teamName, String country, int foundYear){
+        this.teamName = teamName;
+        this.country = country;
+        this.foundYear = foundYear;
+
+    }
+    // Getters and Setters
+    public
 
 }
