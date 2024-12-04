1055b90f8d183c299f7e59930cb6f4df04e9f678
DevlinOT <148062566+DevlinOT@users.noreply.github.com>
2024-12-04 12:25:50 +0000

    getters and setters

--- src/ie/atu/project/Team.java
+++ src/ie/atu/project/Team.java
@@ -3,7 +3,7 @@ package ie.atu.project;
 public class Team {
     private String teamName;
     private String country;
-    private String foundYear;
+    private int foundYear;
 
     public Team(String teamName, String country, int foundYear){
         this.teamName = teamName;
@@ -12,6 +12,13 @@ public class Team {
 
     }
     // Getters and Setters
-    public
+    public String getTeamName() {return teamName; }
+    public void setTeamName(String teamName) { this.teamName = teamName;}
+
+    public String getCountry() {return country; }
+    public void setCountry(String country) { this.country = country;}
+
+    public int getFoundedYear() {return foundYear; }
+    public void setFoundedYear(String foundYear) { this.foundYear = foundYear;}
 
 }
